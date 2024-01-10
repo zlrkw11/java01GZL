280be37f4e88fca1e5077cb50c0d90672b0dad59
zlrkw11 <ray040406@gmail.com>
2024-01-10 19:08:36 +0800

    wrapper class autoboxing & unboxing

---
+++ 1/wrapper_classes.java
@@ -0,0 +1,20 @@
+public class wrapper_classes {
+    public static void main(String[]args){
+        
+        // Autoboxing
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Ray";
+
+        // Unboxing features
+        if (a==true){
+            System.out.println("This is true");
+        }
+
+        
+
+
+    }
+}
