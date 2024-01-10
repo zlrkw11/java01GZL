bbc1320ee3e246b52cf5edd48ea4dd3950846bd9
zlrkw11 <ray040406@gmail.com>
2024-01-10 12:29:25 +0800

    adding 2d array hard writing

--- 1/nested_array.java
+++ 1/nested_array.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 public class nested_array {
 
 	public static void main(String[] args) {
-	
-		String [][] cars = new String [3][3];
 		
-		cars[0][0] = "Camero";
-		cars[0][1] = "Corvette";
-		cars[0][2] = "Silverado";
-		cars[1][0] = "Mustang";
-		cars[1][1] = "Ranger";
-		cars[1][2] = "F-150";
-		cars[2][0] = "Ferrari";
-		cars[2][1] = "Lambo";
-		cars[2][2] = "Tesla";
+
+		String [][] cars = { 
+			
+							 {"Camero", "Corvette", "Silverado"}, 
+							 {"Mustang","Ranger","F-150"}, 
+							 {"Ferrari", "Lambo", "Tesla"}
+							
+							};
+		
 		
 		for (int i=0; i<cars.length; i++) {
 			System.out.println();
