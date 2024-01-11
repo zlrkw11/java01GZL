f8badde2e978a1893685e1f7b7fcae645f35b10b
zlrkw11 <ray040406@gmail.com>
2024-01-11 12:25:43 +0800

    method return types

--- 1/method.java
+++ 1/method.java
@@ -3,13 +3,32 @@ public class method {
 
         // parameter call
         String name = "Ray";
-        hello(name);
+        int age = 19;
+        hello(name, age);
+
+
+        // add method
+        int x = 3;
+        int y = 4;
+
+        int z = add(x, y);
+        System.out.println(z);
+
+        System.out.println(add(x, y));
 
 
     }
 
     // individual hello method
-    static void hello(String a){
-        System.out.println("Hello " +a);
+    static void hello(String name, int age){
+        System.out.println("Hello " +name);
+        System.out.println("You are "+age);
+
+    }
+
+    static int add(int x, int y){
+        int z = x + y;
+        return z;
     }
 }
+
