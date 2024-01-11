7aec47e5ad3d550a9b5779a6e967cea20b6e8426
zlrkw11 <ray040406@gmail.com>
2024-01-11 11:38:28 +0800

    for each + ArrayList

--- 1/for_each_loop.java
+++ 1/for_each_loop.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 public class for_each_loop {
     public static void main(String[]args){
        
         // array
         String [] animals = {"cat", "dog", "rat", "bird"};
 
+        ArrayList<String> animals2 = new ArrayList<String>();
+
+        animals2.add("cat");
+        animals2.add("dog");
+        animals2.add("rat");
+        animals2.add("bird");
+
         // for each loop 
-        for (String i : animals) {
-            System.out.println(i)
+        for (String i : animals2) {
+            System.out.println(i);
         }
     }
 }
