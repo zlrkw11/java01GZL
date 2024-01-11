d27e6e660d8dbc5de54ebbd0a87c0445aa42211d
zlrkw11 <ray040406@gmail.com>
2024-01-11 11:33:37 +0800

    for each loop format

---
+++ 1/for_each_loop.java
@@ -0,0 +1,12 @@
+public class for_each_loop {
+    public static void main(String[]args){
+       
+        // array
+        String [] animals = {"cat", "dog", "rat", "bird"};
+
+        // for each loop 
+        for (String i : animals) {
+            System.out.println(i)
+        }
+    }
+}
