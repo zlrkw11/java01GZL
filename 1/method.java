55af7c74887786b339be9caf27cedb0f9f9fe806
zlrkw11 <ray040406@gmail.com>
2024-01-11 12:14:44 +0800

    method arguments

---
+++ 1/method.java
@@ -0,0 +1,15 @@
+public class method {
+    public static void main(String[]args){
+
+        // parameter call
+        String name = "Ray";
+        hello(name);
+
+
+    }
+
+    // individual hello method
+    static void hello(String a){
+        System.out.println("Hello " +a);
+    }
+}
