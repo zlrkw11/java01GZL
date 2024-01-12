517fe793f0bbd6abfb9b314b7841e09179b4de56
zlrkw11 <ray040406@gmail.com>
2024-01-12 12:29:39 +0800

    width property

--- 1/printf.java
+++ 1/printf.java
@@ -2,7 +2,23 @@ public class printf {
     public static void main(String[]args){
 
         System.out.printf("This is a format string %d", 123);
-    
+        
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Ray";
+        int myInt = 50; 
+        double myDouble = 1000; 
+
+        System.out.printf("%b", myBoolean);
+        System.out.printf("%c", myChar);
+        System.out.printf("%s", myString);
+        System.out.printf("%d", myInt);
+        System.out.printf("%f", myDouble);
+
+
+        // width property 
+        System.out.printf("Hello %10s", myString);
+        
     }
     
 }
