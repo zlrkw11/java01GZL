52fb9b8de193bfa19ffc8108b83cce6faf2cba98
zlrkw11 <ray040406@gmail.com>
2024-01-12 15:11:00 +0800

    final keyword

---
+++ 1/final_keyword.java
@@ -0,0 +1,12 @@
+public class final_keyword {
+    public static void main(String[]args){
+
+        // anything declared as final cannot be updated
+        final double pi = 3.14159;
+
+        System.out.println(pi);
+
+
+    }
+    
+}
