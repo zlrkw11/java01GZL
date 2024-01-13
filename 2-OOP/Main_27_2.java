3ae2b7d785a6e367b4886ec292b8558a3fa5ee5e
zlrkw11 <ray040406@gmail.com>
2024-01-13 14:42:13 +0800

    constructor example (separate files)

---
+++ 2-OOP/Main_27_2.java
@@ -0,0 +1,11 @@
+public class Main_27_2 {
+    public static void main(String[]args){
+
+        Human human1 = new Human("Nate", 20, 50);
+        Human human2 = new Human("Ray", 19, 63);
+
+        human1.drink();
+        human2.eat();
+
+    }
+}
