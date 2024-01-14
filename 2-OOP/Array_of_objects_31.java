37a4f8f1a5d1c535b63110ddcb922bf2292e9ad1
zlrkw11 <ray040406@gmail.com>
2024-01-14 12:24:40 +0800

    array of objects

---
+++ 2-OOP/Array_of_objects_31.java
@@ -0,0 +1,21 @@
+public class Array_of_objects_31 {
+    public static void main(String[] args) {
+        
+        // data types
+        //Food[] refrigerator = new Food[3];
+
+        Food food1 = new Food("pizza");
+        Food food2 = new Food("hamburger");
+        Food food3 = new Food("hotdog");
+
+
+        // method 2
+        Food[] refrigerator = {food1,food2,food3};
+
+        //refrigerator[0] = food1;
+        //refrigerator[1] = food2;
+        //refrigerator[2] = food3;
+
+        System.out.println(refrigerator[0].name);
+    }
+}
--- 2-OOP/ToString_30.java
+++ 2-OOP/ToString_30.java
@@ -2,5 +2,6 @@ public class ToString_30 {
     public static void main(String[] args) {
         Car car = new Car();
         System.out.println(car.toString());
+        System.out.println(car);
     }
 }
