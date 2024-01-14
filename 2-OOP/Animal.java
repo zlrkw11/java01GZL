64b6d855dfe733eee74dd502dd689a941047f722
zlrkw11 <ray040406@gmail.com>
2024-01-15 00:05:39 +0800

    .

---
+++ 2-OOP/Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+    
+    void speak(){
+
+        System.out.println("The animal speaks");
+        
+    }
+}
---
+++ 2-OOP/Method_overriding.java
@@ -0,0 +1,14 @@
+public class Method_overriding {
+    
+    public static void main(String[] args) {
+        
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+
+        animal.speak();
+        dog.speak();
+
+    }
+}
+
+
