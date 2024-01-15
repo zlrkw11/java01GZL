9073676a1e730af68c748bc162d9f4bed3718e44
zlrkw11 <ray040406@gmail.com>
2024-01-15 13:36:08 +0800

    super class passing args

---
+++ 2-OOP/Hero.java
@@ -0,0 +1,9 @@
+public class Hero extends Person {
+    String power;
+
+    Hero(String name, int age, String power){
+        super(name, age);
+
+        this.power = power;
+    }
+}
---
+++ 2-OOP/Super_keyword.java
@@ -0,0 +1,8 @@
+public class Super_keyword {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("Batman", 42, "$$$");
+        System.out.println(hero1.name);
+        System.out.println(hero1.age);
+        System.out.println(hero1.power);
+    }
+}
