abd8f595ed42d2d8d0083c915bc6b53b58a39b51
zlrkw11 <ray040406@gmail.com>
2024-01-16 16:32:35 +0800

    a fish class that apply both prey and predator interfaces

--- 2-OOP/Interface/Fish.java
+++ 2-OOP/Interface/Fish.java
@@ -1,5 +1,12 @@
 package Interface;
 
-public class Fish {
+public class Fish implements Prey, Predator {
     
+    public void hunt(){
+        System.out.println("The fish is hunting smaller fish");
+    }
+
+    public void flee(){
+        System.out.println("The fish is fleeing from a bigger fish");
+    }
 }
