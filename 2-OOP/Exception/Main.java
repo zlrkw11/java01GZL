bd4c44e74437eaa7eab97f3e142287734ba49916
zlrkw11 <ray040406@gmail.com>
2024-01-16 20:04:04 +0800

    try and Exception

---
+++ 2-OOP/Exception/Main.java
@@ -0,0 +1,29 @@
+package Exception;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        // exception handling
+        try{
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter a whole number to divide: ");        
+            int x = scanner.nextInt();
+    
+            System.out.println("Enter a whole number to divide by: ");
+            int y = scanner.nextInt();
+    
+            int z = x/y;
+    
+            System.out.println("Result: "+z);
+
+        }
+        catch(ArithmeticException e){
+            System.out.println("You can't divide by zero! IDIOT!");
+        }
+
+
+    }   
+}
