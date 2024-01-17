86940b598ce8e064f962b8cd20e8269d5c45ea15
zlrkw11 <ray040406@gmail.com>
2024-01-17 23:16:46 +0800

    trick: setDefaultCloseOperation .EXIT_ON_CLOSE

--- 2-OOP/GUI/Main.java
+++ 2-OOP/GUI/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         
-
+        // setting up 
         frame.setTitle("zl11_KAEDE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setVisible(true); // make frame visible
     
