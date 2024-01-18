e0cd6c8dafff83e46dfe23433f53411514e3a41e
zlrkw11 <ray040406@gmail.com>
2024-01-18 15:06:29 +0800

    ImageIcon add

--- 2-OOP/Label/Main.java
+++ 2-OOP/Label/Main.java
@@ -1,11 +1,15 @@
 package Label;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.ImageIcon;
 public class Main {
     public static void main(String[] args) {
+
+        ImageIcon image = new ImageIcon("2-OOP\\Label\\sd.jpg");
+
         JLabel label = new JLabel();
         label.setText("Bro, do you even code?");
+        label.setIcon(image);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
