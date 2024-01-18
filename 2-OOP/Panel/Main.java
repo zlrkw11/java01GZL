6b7a08751e4b58ca432fc3051665e7d7359d7810
zlrkw11 <ray040406@gmail.com>
2024-01-18 23:00:01 +0800

    set color and position & dimension

--- 2-OOP/Panel/Main.java
+++ 2-OOP/Panel/Main.java
@@ -1,12 +1,14 @@
 package Panel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+import java.awt.Color;
 public class Main {
 
     public static void main(String[] args) {
 
         JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250)
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,6 +16,7 @@ public class Main {
         frame.setLayout(null);
         frame.setVisible(true);
 
+        JFrame.add(redPanel);
 
 
     }
