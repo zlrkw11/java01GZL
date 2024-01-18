5a87e51e407a37c8035c8c52f245c4c5a6f946b8
zlrkw11 <ray040406@gmail.com>
2024-01-18 22:56:57 +0800

    import JPanel

--- 2-OOP/Panel/Main.java
+++ 2-OOP/Panel/Main.java
@@ -1,13 +1,20 @@
 package Panel;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        JPanel redPanel = new JPanel();
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(750,750);
         frame.setLayout(null);
         frame.setVisible(true);
-        
+
+
+
     }
 }
