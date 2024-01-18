8688e47b021d8a4122c54fd1ac653040cff1cc31
zlrkw11 <ray040406@gmail.com>
2024-01-18 15:23:14 +0800

    set the label + text position

--- 2-OOP/Label/Main.java
+++ 2-OOP/Label/Main.java
@@ -1,21 +1,42 @@
 package Label;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.border.Border;
 public class Main {
     public static void main(String[] args) {
 
-        ImageIcon image = new ImageIcon("2-OOP\\Label\\sd.jpg");
+        ImageIcon image = new ImageIcon("2-OOP\\Label\\sh.jpeg");
+        Border border = BorderFactory.createLineBorder(Color.green,3);
 
         JLabel label = new JLabel();
-        label.setText("Bro, do you even code?");
+        label.setText("I'm a Sportsman!");
         label.setIcon(image);
+
+        // text position
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        //label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(new Color(0x00FF00));
+        label.setFont(new Font("MV Boli", Font.BOLD, 20));
+        label.setIconTextGap(20);
+
+        label.setBackground(Color.black);
+        label.setOpaque(true);
+        label.setBorder(border);
+
+        // change the entire position
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setVisible(true);
         frame.add(label);
-        frame.setResizable(false);
     }
 }
