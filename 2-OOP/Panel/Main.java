54cca1781f48d83de1b8206938fedc0aa8106a0e
zlrkw11 <ray040406@gmail.com>
2024-01-18 22:55:50 +0800

    set up JFrame

---
+++ 2-OOP/Panel/Main.java
@@ -0,0 +1,13 @@
+package Panel;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(750,750);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        
+    }
+}
