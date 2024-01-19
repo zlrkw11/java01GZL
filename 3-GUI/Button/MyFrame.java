04873627aa4b20a68c4e3250ba62143a83d21367
zlrkw11 <ray040406@gmail.com>
2024-01-19 15:19:33 +0800

    import button / set up

--- 3-GUI/Button/MyFrame.java
+++ 3-GUI/Button/MyFrame.java
@@ -1,15 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class MyFrame extends JFrame{
     
     MyFrame(){
 
-        
+        JButton button = new JButton();
+        button.setBounds(200, 100, 100, 50);
+
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
-
+        this.add(button);
     }
 }
