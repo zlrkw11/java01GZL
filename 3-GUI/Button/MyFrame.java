23a359bbadc2b8a2e24aa333a66053f746cdb5e4
zlrkw11 <ray040406@gmail.com>
2024-01-19 15:35:06 +0800

    ActionListener method to trigger the button

--- 3-GUI/Button/MyFrame.java
+++ 3-GUI/Button/MyFrame.java
@@ -1,18 +1,32 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.*;
 
-public class MyFrame extends JFrame{
-    
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MyFrame extends JFrame implements ActionListener{
+    JButton button;
     MyFrame(){
 
-        JButton button = new JButton();
+        button = new JButton();
         button.setBounds(200, 100, 100, 50);
 
+        //button.addActionListener(e -> System.out.println("hi"));
+        button.addActionListener(this);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
         this.add(button);
+
+        
     }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==button){
+            System.out.println("hi");
+        }
+    } 
 }
