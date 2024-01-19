25648374437c8e21ef1773166359ed7718064a66
zlrkw11 <ray040406@gmail.com>
2024-01-19 15:18:06 +0800

    set up

--- 3-GUI/Button/MyFrame.java
+++ 3-GUI/Button/MyFrame.java
@@ -2,4 +2,14 @@ import javax.swing.JFrame;
 
 public class MyFrame extends JFrame{
     
+    MyFrame(){
+
+        
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
+
+    }
 }
